89ea2aba6138ea59de5ed48b51775f0ba49c9ffc
Peter Collingridge <peter.collingridge@nanoporetech.com>
2023-06-25 16:38:27 +0100

    Add player class

---
+++ scripts/player.js
@@ -0,0 +1,13 @@
+class Player {
+  constructor(position, groups) {
+    this.position = position;
+    this.groups = groups;
+    this.image = document.getElementById(`img-player`)
+  }
+
+  update(dt) {
+  }
+
+  draw(ctx) {
+  }
+}
